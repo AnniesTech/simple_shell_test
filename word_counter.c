35a6f88b4a116193365a818d10ccba7f43f21e55
Ana Rocha <anita.rocha0505@gmail.com>
2021-12-01 17:35:37 -0500

    betty for word counter

--- word_counter.c
+++ word_counter.c
@@ -1,22 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+/**
+ * word_counter - counts each letter from the string
+ *@str: string
+ * Return: the result
+ */
+
 int word_counter(char *str)
 {
-    unsigned int wc;
-    int estado = 0;
+	unsigned int wc;
+	int estado = 0;
 
-    while (*str)
-    {
-        if (*str == ' ')
-        {
-            estado = 0;
-        }
-        else if (estado == 0)
-        {
-            estado = 1;
-            ++wc;
-        }
-        ++str;
-    }
-    return (wc);
+	while (*str)
+	{
+		if (*str == ' ')
+		{
+			estado = 0;
+		}
+		else if (estado == 0)
+		{
+			estado = 1;
+			++wc;
+		}
+		++str;
+	}
+	return (wc);
 }
