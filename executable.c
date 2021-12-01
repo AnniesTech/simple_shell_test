9f30b029bfe21c85b940f6d0dc344b066c0ce7f7
Ana Rocha <anita.rocha0505@gmail.com>
2021-12-01 17:36:58 -0500

    betty for executable

--- executable.c
+++ executable.c
@@ -1,27 +1,34 @@
 #include "main.h"
+
+/**
+ * executable - Executes the child process
+ * @args: arguments
+ * Return: the result
+ */
+
 int executable(char **args)
 {
-    pid_t pid;
-    int status;
+	pid_t pid;
+	int status;
 
-    pid = fork();
-    if (pid == -1)
-    {
-        perror("No such file or directory\n");
-        exit(EXIT_FAILURE);
-    }
-    if (pid == 0)
-    {
-        if (execve(args[0], args, NULL) == -1)
-        {
-            perror("Couldn't execute the program");
-            exit(EXIT_FAILURE);
-        }
-    }
-    else
-    {
-        wait(&status);
-        printf("Waiting for child");
-    }
-    return (1);
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("No such file or directory\n");
+		exit(EXIT_FAILURE);
+	}
+	if (pid == 0)
+	{
+		if (execve(args[0], args, NULL) == -1)
+		{
+			perror("Couldn't execute the program");
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		wait(&status);
+		printf("Waiting for child");
+	}
+	return (1);
 }
