ff4d3904fbf61b6561723b0c04ed6b4011400017
Ana Rocha <anita.rocha0505@gmail.com>
2021-12-01 17:30:23 -0500

    fixed betty for auxiliares file

--- auxiliares.c
+++ auxiliares.c
@@ -1,55 +1,79 @@
 #include "main.h"
 
+/**
+ * _strlen - Function that returns the lenght of a string.
+ * @n: String
+ * Return: the result
+ */
+
 int _strlen(char *n)
 {
-    int i = 0;
+	int i = 0;
 
-    while (n[i])
-    {
-        i++;
-    }
-    return (i);
+	while (n[i])
+	{
+		i++;
+	}
+	return (i);
 }
+
+/**
+ * _strdup - Function that returns the duplicate of a string
+ * @str: string
+ * Return: the result
+ */
 
 char *_strdup(char *str)
 {
-    int len = _strlen(str), i;
-    char *dest;
+	int len = _strlen(str), i;
+	char *dest;
 
-    if (str == NULL)
-    {
-        return (NULL);
-    }
-    dest = malloc(sizeof(char) * (len + 1));
-    if (dest == NULL)
-    {
-        free(dest);
-        return (NULL);
-    }
-    for (i = 0; i < len + 1; i++)
-    {
-        dest[i] = str[i];
-    }
-    return (dest);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	dest = malloc(sizeof(char) * (len + 1));
+	if (dest == NULL)
+	{
+		free(dest);
+		return (NULL);
+	}
+	for (i = 0; i < len + 1; i++)
+	{
+		dest[i] = str[i];
+	}
+	return (dest);
 }
+
+/**
+ * grid_size - Checks for the size of the grid
+ * @grid: grid
+ * Return: the result
+ */
 
 int grid_size(char **grid)
 {
-    int i = 0;
+	int i = 0;
 
-    while (*grid)
-    {
-        i++;
-        grid++;
-    }
-    return (i);
+	while (*grid)
+	{
+		i++;
+		grid++;
+	}
+	return (i);
 }
+
+/**
+ * free_grid - Frees the space on memory for the grid
+ * @grid: grid
+ * @height: height
+ */
 
 void free_grid(char **grid, int height)
 {
-    int i;
+	int i;
 
-    for (i = 0; i < height; i++)
-        free(grid[i]);
-    free(grid);
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
 }
