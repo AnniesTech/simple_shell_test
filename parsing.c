8d235f7fad271ab61f16295f4a24a37f178dde12
DaboRestrepo <daborestrepo@gmail.com>
2021-12-01 17:34:32 -0500

    Fix Betty

--- parsing.c
+++ parsing.c
@@ -1,27 +1,27 @@
 #include "main.h"
 /**
  * token_1 - Function that tokenize the user string.
- * @str: user string. 
- * Return: the split of the string. 
+ * @str: user string.
+ * Return: the split of the string.
  */
 char **token_1(char *str)
 {
-    unsigned int wc = word_counter(str), pos = 0;
-    char **split = malloc(sizeof(char *) * wc);
+	unsigned int wc = word_counter(str), pos = 0;
+	char **split = malloc(sizeof(char *) * wc);
 
-    if (split == NULL)
-    {
-        free(split);
-        exit(EXIT_FAILURE);
-    }
-    split[pos] = _strdup(strtok(str, " \n\t"));
-    pos++;
+	if (split == NULL)
+	{
+		free(split);
+		exit(EXIT_FAILURE);
+	}
+	split[pos] = _strdup(strtok(str, " \n\t"));
+	pos++;
 
-    while (pos < wc - 1)
-    {
-        split[pos] = _strdup(strtok(NULL, " \n\t"));
-        pos++;
-    }
-    split[pos] = NULL;
-    return (split);
+	while (pos < wc - 1)
+	{
+		split[pos] = _strdup(strtok(NULL, " \n\t"));
+		pos++;
+	}
+	split[pos] = NULL;
+	return (split);
 }
