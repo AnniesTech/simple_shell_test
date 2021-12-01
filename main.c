2691a2edc7a6bfce093873951c87f2603aa5ecfb
Ana Rocha <anita.rocha0505@gmail.com>
2021-12-01 17:39:01 -0500

    betty for all

--- main.c
+++ main.c
@@ -1,9 +1,17 @@
 #include "main.h"
+
+/**
+ * main -  Shell
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: always success
+ */
+
 int main(int argc, char **argv)
 {
-    (void)argc;
-    (void)argv;
-    prompt();
+	(void)argc;
+	(void)argv;
+	prompt();
 
-    return EXIT_SUCCESS;
+	return (EXIT_SUCCESS);
 }
